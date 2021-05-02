1bd1c9a933e7249a7e0e707fd1c2882d24d8deb1
Hiteke <38786753+Hiteke@users.noreply.github.com>
2021-05-02 16:55:21 +0900

    Add files via upload

---
+++ Server/UnityDebugServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UnityDebugServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 7777);
+			
+            Console.WriteLine("DEBUG Server started!");
+            server.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Waiting... ");
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected...");
+                NetworkStream stream = client.GetStream();
+
+                while (client.Connected)
+                {
+                    byte[] data = new byte[256];
+                    int bytes = stream.Read(data, 0, data.Length);
+
+                    if (bytes != 0)
+                    {
+
+                        string message = Encoding.UTF8.GetString(data, 0, bytes);
+                        Console.WriteLine(message);
+                    }
+
+                    if (client.Client.Poll(0, SelectMode.SelectRead))
+                    {
+                        byte[] buff = new byte[1];
+                        if (client.Client.Receive(buff, SocketFlags.Peek) == 0)
+                        {
+                            client.Close();
+                        }
+                    }
+                }
+                Console.WriteLine("Disconnected...");
+            }
+        }
+    }
+}
